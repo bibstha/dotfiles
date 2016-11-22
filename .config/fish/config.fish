b94a7062ecdb3b3fa2470ed5b18253ba687a2b04
Bibek Shrestha <bibek.shrestha@runtastic.com>
2016-11-22 12:33:43 +0100

    Remove omfish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,32 +1,18 @@
-# Path to Oh My Fish install.
-set -gx OMF_PATH $HOME/.local/share/omf
-
-# Customize Oh My Fish configuration path.
-set -gx OMF_CONFIG $HOME/.config/omf
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
-source $HOME/.config/omf/alias.fish
-
+# Chruby
 if test -d /usr/local/share/chruby
   source /usr/local/share/chruby/chruby.fish
   source /usr/local/share/chruby/auto.fish
 end
 
-if test -d /Applications/Postgres.app/Contents/Versions/9.4/bin
-  set -gx PATH $PATH /Applications/Postgres.app/Contents/Versions/9.4/bin
-end
-
+# Gnu Coreutils
 if test -d /usr/local/opt/coreutils/libexec/gnubin
   set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
 end
 
+# Common env variables
 set -gx JRUBY_OPTS --dev
-
 set -gx EDITOR /usr/local/bin/vim
-
 set -gx GOPATH /Users/bis/Projects/go
-
 set -gx PATH /usr/local/sbin $PATH $GOPATH/bin
 
 # this function may be required
