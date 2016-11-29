45d61b57430b720a39f0cdc52623ff881628aaba
Bibek Shrestha <bibek.shrestha@runtastic.com>
2016-11-29 10:58:21 +0100

    Add missing alias file

---
+++ .config/omf/alias.fish
@@ -0,0 +1,12 @@
+# alias specific
+balias a 'vim $OMF_CONFIG/alias.fish'
+balias sa 'source $OMF_CONFIG/alias.fish'
+
+balias pr 'stash pull-request'
+
+# git
+balias p 'git pull'
+balias pu 'git push'
+
+# reno
+balias reno 'exe/reno'
