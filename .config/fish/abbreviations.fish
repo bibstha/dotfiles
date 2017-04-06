0f95e9d47c99b3850d644a7a7dc83f89aeb4160c
Bibek Shrestha <bibekshrestha@gmail.com>
2017-04-06 18:42:01 -0400

    Add abbrebiations file

---
+++ .config/fish/abbreviations.fish
@@ -0,0 +1,32 @@
+# from https://github.com/jcelliott/dotfiles/blob/master/.config/fish/abbreviations.fish
+# Set fish user abbreviations
+#
+# More git abbreviations can be borrowed from 
+# https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/git/git.plugin.zsh#L41
+abbr_add 'g=git'
+abbr_add 'gst=git status'
+abbr_add 'gc=git commit -v'
+abbr_add 'glg=git log-detail'
+abbr_add 'glb=git log-branch'
+abbr_add 'glgs=git log-summary'
+abbr_add 'ga=git add'
+
+abbr_add 'gcm=git checkout master'
+abbr_add 'gch=git checkout'
+abbr_add 'gcb=git checkout -b'
+abbr_add 'gd=git diff'
+abbr_add 'gdc=git diff --cached'
+abbr_add 'gds=git diff --stat'
+abbr_add 'gmm=git merge master'
+abbr_add 'gba=git branch -a'
+
+abbr_add 'v=vim'
+abbr_add 'ev=vim ~/.vimrc'
+abbr_add 'et=vim ~/.tmux.conf'
+abbr_add 'efc=vim ~/.config/fish/config.fish'
+abbr_add 'eff=vim ~/.config/fish/functions'
+abbr_add 'ef=vim ~/.config/fish'
+
+abbr_add 'fc=source ~/.config/fish/config.fish'
+abbr_add 'sf=source ~/.config/fish/config.fish'
+
